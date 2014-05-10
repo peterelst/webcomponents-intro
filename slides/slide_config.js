957ce117d2c8a8b0e6bef5367cd007be0f799fd7
Peter Elst <peterelst@google.com>
2014-05-10 00:07:05 -0700

    initial commit slides

---
+++ slides/slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Web Components',
+    subtitle: 'Building Blocks for the Web',
+    eventInfo: {
+      title: 'Multi-Mania',
+      date: 'May 20th 2014'
+    },
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Peter Elst',
+    company: 'Web Solutions Engineer, Creative Innovation - Google',
+    gplus: 'http://www.google.com/+peterelst',
+    twitter: '@peterelst',
+    www: 'http://www.peterelst.com',
+    github: 'http://github.com/peterelst'
+  }]
+};
+
